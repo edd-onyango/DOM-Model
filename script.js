003346687e3666952b8b6e130ff0e26df102edf5
edd-onyango <eddonyango.dev@gmail.com>
2024-01-09 13:19:25 +0300

    Add elements using js to the DOM

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 const container  = document.querySelector('#container');
 // Selects the #container div
 
-console.dir(container.firstElementChild);
-//Selects the first child of the #container => .display
+const content = document.createElement('div');
+content.classList.add('content');
 
-const controls  = document.querySelector('.controls');
-//Select the .controls div
+content.textContent = 'This is the glorious text-content!';
 
-console.dir(controls.previousElementSibling);
-// Selects the prior sibling => .display
+container.appendChild(content);
+
+//Red Paragraph
+const paragraph = document.createElement('p');
+paragraph.classList.add('red-text');
+
+paragraph.textContent = 'Hey I am red.';
+
+paragraph.style.cssText = "color:red";
+
+container.appendChild(paragraph);
+
+
+//Blue Heading
+const heading3 = document.createElement('h3');
+heading3.classList.add('blue-h3');
+
+heading3.textContent = 'I am a blue h3 Heading';
+
+heading3.style.cssText = "color:blue";
+
+container.appendChild(heading3);
+
+//Black border div
+const pinkContent = document.createElement('div');
+pinkContent.classList.add('black-border-pink');
+
+pinkContent.style.cssText = 'border:1px solid black; background-color:pink';
+
+const header1 = document.createElement('h1');
+header1.textContent = 'I am in a div';
+
+const paragraphOne = document.createElement('p');
+paragraphOne.textContent = 'ME TOO!';
+
+pinkContent.append(header1, paragraphOne);
+
+container.appendChild(pinkContent);
